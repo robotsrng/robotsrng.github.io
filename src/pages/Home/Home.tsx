1a7908f27a7e07f2a24da3f591e21deb448b9dbe
robotsrng <shane@shattered.dev>
2024-10-05 19:32:08 +1000

    Name

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,13 +7,12 @@ const Home = () => {
     <Container sx={{ py: 2, position: 'relative' }}>
       <Stack gap={1} my={2}>
         <Typography textAlign="center" variant="h2">
-          Vite-MUI-TS Template
+          Shane Glass
         </Typography>
         <Typography textAlign="center" variant="subtitle1">
-          React + TS + Vite + Redux + RTK + MUI + RRD + Prettier
+Australia
         </Typography>
       </Stack>
-      <TemplateTester />
       <Counter />
     </Container>
   );
--- src/routes/Routing.tsx
+++ src/routes/Routing.tsx
@@ -1,14 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
-import Register from '@/pages/Auth/Register';
-import Login from '@/pages/Auth/Login';
 import Home from '@/pages/Home/Home';
 
 const Routing = () => {
   return (
     <Routes>
       <Route path="*" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
     </Routes>
   );
 };
